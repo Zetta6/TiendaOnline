c8668f50920adb54e1a6f3ff07816b5afb0dbbbc
RodrigoAVV <rodrigo.vidal@virginiogomez.cl>
2021-10-29 11:59:29 -0300

    Se mejora layout

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -18,20 +18,20 @@
 				Inicio menú de navegación
 		
 			-->
-			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-	
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Buscar</button>
-      </form>
-    </div>
-  </div>
-</nav>
+					<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		  <div class="container-fluid">
+			<a class="navbar-brand" href="#">Navbar</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			  <span class="navbar-toggler-icon"></span>
+			</button>
+			
+			  <form class="d-flex">
+				<input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+				<button class="btn btn-outline-success" type="submit">Buscar</button>
+			  </form>
+			</div>
+		  
+		</nav>
 
 			<!-- 
 		
@@ -85,6 +85,35 @@
 		
 		-->
     <script src="{{ url('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js') }}" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<footer class="text-center text-white" style="background-color: #45637d;">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!-- Section: Iframe -->
+    <section class="">
+      <div class="row d-flex justify-content-center">
+        <div class="col-lg-6">
+          <div class="ratio ratio-16x9">
+            <iframe
+              class="shadow-1-strong rounded"
+              src="https://www.youtube.com/embed/vlDzYIIOYmM"
+              title="YouTube video"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Section: Iframe -->
+  </div>
+  <!-- Grid container -->
 
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+</div>
 	</body>
 </html>
--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,1 +1,31 @@
-Hola que tal
+@extends ('layout.layout')
+@section('title','Listado de productos')
+@section('content')
+	<table class="table">
+		<tr>
+			<th scope="col">Nro</th>
+			<th scope="col">Nombre</th>
+			<th scope="col">apellido</th>
+			<th scope="col">contacto</th>
+		</tr>
+		<tr>
+			<th scope="row">1</th>
+			<td>Mark</td>
+			<td>Otto</td>
+			<td>themarck@gmail.com</td>
+		</tr>
+		<tr>
+			<th scope="row">2</th>
+			<td>Jacob</td>
+			<td>Thornton</td>
+			<td>jacoboth@gmail.com</td>
+		</tr>
+			<tr>
+			<th scope="row">3</th>
+			<td>larry</td>
+			<td>olsonn</td>
+			<td>theolsonn@gmail.com</td>
+				
+		</tr>
+	</table>
+@endsection
